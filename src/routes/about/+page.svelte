8d3df89e37fbf8ed57d3948e821fab7d4bc6e7e7
Jordan Martin <jordan.martin2890@gmail.com>
2024-04-12 20:32:57 -0500

    Uploaded Blog1, updated Footer, Updated About Page, Updated Blog Page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,11 +6,11 @@
 		Accruent. In my spare time, I work on learning new technologies for this blog, teach at my local
 		church, and run a YouTube channel (gaming content).
 	</p>
+	<p>My work experiences have</p>
 	<p>
 		Before entering the world of development, I worked at Jacobs Engineering Group and started a
 		global IT support desk. We supported 65,000+ users world-wide.
 	</p>
-	<p>My work experiences have always</p>
 	<p>
 		Personally, I live in Livingston, Louisiana. I'm married, with no kids yet. I have a dog named
 		Rally. I love sports, specifically college sports. Geaux Tigers.
@@ -23,5 +23,8 @@
 		align-items: left;
 		flex-direction: column;
 		margin: 0 25%;
+		& h1 {
+			margin-bottom: 2rem;
+		}
 	}
 </style>
